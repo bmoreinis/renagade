c1421fd15b00f3715906b510940ab749498af361
Bram Moreinis <bmoreinis@gmail.com>
2022-04-03 17:14:43 +0000

    Added Satire Notice

---
+++ script.js
@@ -0,0 +1,3 @@
+function thisSatire(){
+  alert("This article is satire. Satire is fiction that mimics the style of a news story, exaggerating the truth to make a point. \n\nThis story below is made up, but it makes fun of real things we are dealing with in our world, and it links to a real news story. \n\nPlease enjoy it, and also think about it!");
+}
